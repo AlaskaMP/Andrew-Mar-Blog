7d92ca7b07f4c082b86bf2237c41ac1f9e6bb0c2
alaskaMP <alexandermarportal@gmail.com>
2023-12-11 00:02:07 -0500

    final version

---
+++ src/data/information.js
@@ -0,0 +1,20 @@
+export const information=[
+    {
+        title:"React Shop",
+        description:"Tienda virtual gestionada por el hook useContext de React. La tienda filtra productos segun categoria y precios, también el usuario tiene un carrito de compras donde puede ver sus articulos así como añadir o eliminar los articulos.",
+        link:"https://github.com/AlaskaMP/React/tree/main/projects/React%20Shop",
+        img:"../images/Shop.png"
+    },
+    {
+        title:"Buscador de Peliculas",
+        description:"Buscador de peliculas que consume la API de OMDb con diseño responsive y optimización con los hooks useMemo y useCallback de React",
+        link:"https://github.com/AlaskaMP/React/tree/main/projects/Peliculas",
+        img:"../images/Peliculas.png"
+    },
+    {
+        title:"Tres en Raya",
+        description:" El proyecto Tres en Raya es una implementación del clásico juego de mesa. La gestión del juego se realiza de manera eficiente mediante el uso y propiedades de React. ",
+        link:"https://github.com/AlaskaMP/React/tree/main/projects/Tres%20en%20raya",
+        img:"../images/Juego.png"
+    }
+]
